5570ec797063d7efdbd808f71d30ccccc446bcc3
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-06-09 19:42:58 -0600

    switch statements for prompt and functions formatted

--- server.js
+++ server.js
@@ -11,7 +11,39 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the books_db database.`)
 );
+mainMenu();
 
+//put the initial promts in a function
+function mainMenu() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        //structure each option as a function then call for it in swithc/if statement
+        "View All Employees",
+        "Add Employees",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Quit",
+      ],
+      name: "mainMenu",
+    },
+  ]);
+}
+//async await
+function viewAllEmp() {
+  //db.query
+}
+
+function addEmp() {}
+
+function updateEmpRole() {}
+
+functoin;
 // db.query("SELECT COUNT(id) AS ", function (err, results) {
 //   console.log(results);
 // });
@@ -22,21 +54,3 @@ const db = mysql.createConnection(
 //     console.log(results);
 //   }
 // );
-
-inquirer.prompt([
-  {
-    type: "list",
-    message: "What would you like to do?",
-    choices: [
-      "View All Employees",
-      "Add Employees",
-      "Update Employee Role",
-      "View All Roles",
-      "Add Role",
-      "View All Departments",
-      "Add Department",
-      "Quit",
-    ],
-    name: "mainMenu",
-  },
-]);
